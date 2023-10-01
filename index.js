a0b4c8f3c993b461e5cf5a439047413e54bbd2dc
justmann <mj.justmann@gmail.com>
2023-10-01 14:08:01 -0500

    updates-questions-filepath

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const promptUser = require("./inquirer");
 promptUser()
   .then((answers) => {
     console.log(answers);
+    console.log(answers.text)
+    console.log(answers.textColor)
   })
   .catch((error) => {
     console.log(error);
