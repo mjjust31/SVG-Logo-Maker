8edd371d0e9dd0efe1c2437819823be0b990a843
justmann <mj.justmann@gmail.com>
2023-10-02 13:58:43 -0500

    validation-update

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const promptUser = require("./lib/script-files/inquirer");
+const fs = require("fs");
+const path = require("path");
+
 
 promptUser()
-  .then(({ text, textColor, shape, shapeColor }) => {
-    //destructure the answers object to use user answer for easier code.
-
-    if (text.length > 3 || text.length === 0) {
-      console.log("incorrect length");
-    } else {
-      console.log(text);
-    }
+  .then(({text, textColor, shape, shapeColor}) => {
+    fs.writeFile("shape.svg", data.shape, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
   })
   .catch((error) => {
     console.log(error);
   });
+
+function Shape(text, textColor, shape, shapeColor) {
+  this.text = text;
+  this.textColor = textColor;
+  this.shape = shape;
+  this.shapeColor = shapeColor;
+}
+
+function renderCircle(text, textColor, shape, ShapeColor, circle) {
+  Shape.call(this, text, textColor, shape, shapeColor);
+  this.circle = circle; 
+}
