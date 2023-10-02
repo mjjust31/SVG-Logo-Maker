8e4feceec4e32cbbe739fc3cd0dcdb75182203b0
justmann <mj.justmann@gmail.com>
2023-10-02 14:22:47 -0500

    testing-circle

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const promptUser = require("./lib/script-files/inquirer");
 const fs = require("fs");
 const path = require("path");
 
-
 promptUser()
-  .then(({text, textColor, shape, shapeColor}) => {
-    fs.writeFile("shape.svg", data.shape, (err) =>
-      err ? console.error(err) : console.log("Success!")
-    );
+  .then((answers) => {
+    console.log('answers:',answers);
   })
   .catch((error) => {
     console.log(error);
   });
 
-function Shape(text, textColor, shape, shapeColor) {
-  this.text = text;
-  this.textColor = textColor;
-  this.shape = shape;
-  this.shapeColor = shapeColor;
-}
+// function Shape(text, textColor, shape, shapeColor) {
+//   this.text = text;
+//   this.textColor = textColor;
+//   this.shape = shape;
+//   this.shapeColor = shapeColor;
+// }
 
-function renderCircle(text, textColor, shape, ShapeColor, circle) {
-  Shape.call(this, text, textColor, shape, shapeColor);
-  this.circle = circle; 
-}
+// function renderCircle(text, textColor, shape, ShapeColor, circle) {
+//   Shape.call(this, text, textColor, shape, shapeColor);
+//   this.circle = circle;
+// }
+
+// fs.writeFile("shape.svg", answers, (err) =>
+//   err ? console.error(err) : console.log("Success!")
+// );
